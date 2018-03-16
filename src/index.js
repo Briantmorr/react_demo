29ef97b6a580390c102d25360142d988857d0b7f
briantmorr <briantmorr@gmail.com>
2018-03-16 16:35:26 -0700

    created react-demo repo with react_intro

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Try',
+    lastName: 'Me'
+};
+function greetUser(user){
+    return user.firstName + ' ' + user.lastName;
+}
+const greeting = <h1>Welcome to react: {greetUser(user)}</h1>;
 ReactDOM.render(
-    <App />,
+    // <App />,
+    greeting,
     document.getElementById('root')
 );
