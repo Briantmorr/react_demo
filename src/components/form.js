212cdefab3bdc56d6fbe02717a80b31c0736435d
briantmorr <briantmorr@gmail.com>
2018-03-22 15:20:30 -0700

    finished demo

--- src/components/form.js
+++ src/components/form.js
@@ -5,9 +5,30 @@ class Form extends Component {
         super(props);
 
         this.state = {
-            email: '',
-            password: ''
+            form: {
+                email: '',
+                password: ''
+            }
         };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleFormSubmit(event){
+        event.preventDefault();
+        console.log('submitted state: ', this.state);
+    }
+    handleInputChange(event){
+        const {name, value} = event.target;
+        console.log('Value ', event.target.value);
+        console.log('name:', event.target.name);
+
+        this.setState({
+            form:{
+                ...this.state.form,
+                [name]: value
+                
+            }
+        });
     }
     render() {
         const formStyle = {
@@ -15,19 +36,19 @@ class Form extends Component {
             color: 'grey',
         }
 
-        const {email, password} = this.state;
-
-        return <form>
+        const {email, password} = this.state.form;
+        console.log('state: ', this.state);
+        return <form onSubmit={this.handleFormSubmit.bind(this)}>
                 <h1>I'm a form</h1>
                 <div style={formStyle}>
                     <label>Email</label>
                     <br/>
-                    <input type="text" value={email}/>
+                    <input name='email' type="text" value={email} onChange={event => this.handleInputChange(event)} required/>
                 </div>
                 <div style={formStyle}>
                     <label>Password</label>
                     <br/>
-                    <input  type="password"  value={password}/>
+                    <input  name='password' type="password"  value={password} onChange={event => this.handleInputChange(event)} required/>
                 </div>
                 <input type="submit"/>
                 </form>
