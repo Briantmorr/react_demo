93bff2389472f34210031c09521a21d0db66537d
briantmorr <briantmorr@gmail.com>
2018-04-02 15:02:43 -0700

    done with changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
-
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
-);
+console.dir(React.Component);
+class App extends Component {
+    constructor(props){
+        super(props);
+        console.log('my props are: ', props);
+        var a = 4;
+        console.dir(a);
+    }
+    render() {
+        console.log('my props are: ', this.props);
+        console.log('in render: ', this.a); //undefined
+        return (
+            <div>
+                <div className="app">
+                    <img src={logo} className="logo rotate" />
+                    <h1>Welcome to React {this.a++}{this.a}</h1>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
